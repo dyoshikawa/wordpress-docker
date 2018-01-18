b01cd90d2031da5dc448294cc4cf6a2e08b544e3
amedog <americandog1993@gmail.com>
2018-01-19 00:12:33 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.env
    #	development.env
    #	docker-compose.yml
    #	mysql.env

--- wp/wp-entrypoint.sh
+++ wp/wp-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd /var/www/wp-pre
+wp --allow-root core config --dbhost=mysql --dbuser=root --dbpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_DATABASE} --dbprefix=wp_
+wp --allow-root core install --url=localhost:8000 --title=test --admin_name=test --admin_email=test@test.com --admin_password=password
 mv /var/www/wp-pre/* /var/www/wp
 mv /var/www/wp-pre/.??* /var/www/wp
 rm -rf /var/www/html /var/www/wp-pre
