2e62b06fd43cc427b3570997d5c0090f907fe9be
dyoshikawa <yoshikawada@crunchtimer.jp>
2018-01-19 14:16:56 +0900

    fix

--- README.md
+++ README.md
@@ -23,3 +23,4 @@ $ docker-compose up -d
 - https://joppot.info/2017/05/12/3977
 - https://github.com/github/gitignore/blob/master/WordPress.gitignore
 - https://qiita.com/ry0f/items/6e29fa9f689b97058085
+- https://qiita.com/osktak/items/f1746d64797a6a4dd6ae
--- wp/wp-entrypoint.sh
+++ wp/wp-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-mv /var/www/wp-pre/* /var/www/wp
-mv /var/www/wp-pre/.??* /var/www/wp
-rm -rf /var/www/html /var/www/wp-pre
+sudo mv /var/www/wp-pre/* /var/www/wp
+sudo mv /var/www/wp-pre/.??* /var/www/wp
+sudo rm -rf /var/www/html /var/www/wp-pre
 
-until mysqladmin ping -h mysql --silent; do
+until sudo mysqladmin ping -h mysql --silent; do
     echo 'waiting for mysqld to be connectable...'
     sleep 3
 done
 
-wp --allow-root core config --dbhost=mysql --dbuser=root --dbpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_DATABASE} --dbprefix=wp_ --path=/var/www/wp
-wp --allow-root core install --url=localhost:8000 --title=test --admin_name=test --admin_email=test@test.com --admin_password=password --path=/var/www/wp
-chown -R 1000:1000 /var/www/wp
-php-fpm -F
+sudo wp --allow-root core config --dbhost=mysql --dbuser=root --dbpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_DATABASE} --dbprefix=wp_ --path=/var/www/wp
+sudo wp --allow-root core install --url=localhost:8000 --title=test --admin_name=test --admin_email=test@test.com --admin_password=password --path=/var/www/wp
+sudo chown -R 1000:1000 /var/www/wp
+sudo php-fpm -F
