641f7483e638e2252c84304b26e2b4f3b5476dc4
amedog <americandog1993@gmail.com>
2018-01-27 20:28:41 +0900

    refactor: Dockerfile docker-entrypoint.sh

--- wp/docker-entrypoint.sh
+++ wp/docker-entrypoint.sh
@@ -8,7 +8,8 @@ until sudo mysqladmin ping -h mysql --silent; do
     sleep 3
 done
 
-sudo wp --allow-root core config --dbhost=mysql --dbuser=root --dbpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_DATABASE} --dbprefix=${WORDPRESS_DBPREFIX} --path=/var/www/wp
+sudo wp --allow-root core config --dbhost=mysql --dbuser=root --dbpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_DATABASE} \
+        --dbprefix=${WORDPRESS_DBPREFIX} --path=/var/www/wp
 sudo wp --allow-root core install --url=${WORDPRESS_URL} --title=${WORDPRESS_TITLE} --admin_name=${WORDPRESS_ADMINNAME} \
         --admin_email=${WORDPRESS_EMAIL}  --admin_password=${WORDPRESS_PASSWORD} --path=/var/www/wp
 sudo chown -R 1000:1000 /var/www/wp
